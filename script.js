b30363f2e4465d1c995b4c3442b2fd75118e6be5
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-17 18:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // write your code he
+	let index = s1.toLowerCase().indexOf(s2.toLowerCase());
+	return index;
 }
 
 // Please do not change the code below
